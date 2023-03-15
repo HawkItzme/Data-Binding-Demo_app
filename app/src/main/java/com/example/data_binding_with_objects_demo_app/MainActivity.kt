e6e283f0ef90648fd4517389338150c9f88e6329
Hawk Itzme <smquasim016@gmail.com>
2023-03-15 23:42:59 +0530

    Data Binding with Object code added

--- app/src/main/java/com/example/data_binding_with_objects_demo_app/MainActivity.kt
+++ app/src/main/java/com/example/data_binding_with_objects_demo_app/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.data_binding_with_objects_demo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.data_binding_with_objects_demo_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        var v1  = User(1, "Raj", "123" )
+
+        binding.user = v1
     }
 }
---
+++ app/src/main/java/com/example/data_binding_with_objects_demo_app/User.kt
@@ -0,0 +1,7 @@
+package com.example.data_binding_with_objects_demo_app
+
+data class User(
+    var id: Int,
+    var name: String,
+    var pass: String
+)
